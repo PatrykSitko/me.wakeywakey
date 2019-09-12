633e1b3455cbd627cc272215fd3c55329642d351
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-09-12 05:35:52 +0200

    added volume control

--- src/helpers/soundPlayer.js
+++ src/helpers/soundPlayer.js
@@ -2,12 +2,15 @@ import tick from "../sounds/buttons/tick.mp3";
 import mouseEnterLeave from "../sounds/buttons/mouse-enter-leave.mp3";
 
 export const sounds = { tick, mouseEnterLeave };
-export function playSound(sound, mute) {
+export function playSound(sound, mute, volume) {
+  const soundVolume = typeof volume === "number" ? volume : 0.3;
   if (mute) {
     return true;
   }
   const player = document.createElement("audio");
   player.setAttribute("src", sound);
+  console.log(volume);
+  player.volume = soundVolume;
   player.play();
   return true;
 }
