6bb680309a94ac3ef8df17f7777ea735ce628c42
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-09-14 18:55:08 +0200

    Added mouse enter/leave and twitter sound effects

---
+++ src/components/AuthorNotice.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { playSound, sounds as sound } from "./soundPlayer";
+import "./AuthorNotice.css";
+
+function AuthorNotice({ mute, volume }) {
+  const [clicked, setClicked] = useState(false);
+  return (
+    <p className="author-and-owner-notice">
+      Created by{" "}
+      <span
+        onMouseEnter={playSound.bind(this, sound.mouseEnterLeave, mute, volume)}
+        onMouseLeave={() =>
+          !clicked && playSound(sound.mouseEnterLeave, mute, volume)
+        }
+        onClick={() => {
+          playSound(sound.twitter, mute, volume);
+          setClicked(true);
+          const timeout = setTimeout(
+            () => clearTimeout(timeout) || setClicked(false),
+            1
+          );
+        }}
+      >
+        <a
+          href="https://twitter.com/PatrykSitkoJS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span
+            className="copyright-notice"
+            role="img"
+            aria-label="Copyright notice"
+          >
+            ©️
+          </span>
+          Patryk Sitko
+        </a>
+      </span>
+    </p>
+  );
+}
+
+export default AuthorNotice;
--- src/components/soundPlayer.js
+++ src/components/soundPlayer.js
@@ -3,8 +3,16 @@ import confirm from "../sounds/buttons/confirm.mp3";
 import select from "../sounds/buttons/select.mp3";
 import deselect from "../sounds/buttons/deselect.mp3";
 import mouseEnterLeave from "../sounds/buttons/mouse-enter-leave.mp3";
+import twitter from "../sounds/buttons/twitter-sound-effect.mp3";
 
-export const sounds = { tick, mouseEnterLeave, confirm, select, deselect };
+export const sounds = {
+  tick,
+  mouseEnterLeave,
+  confirm,
+  select,
+  deselect,
+  twitter
+};
 export function playSound(sound, mute, volume) {
   const soundVolume = typeof volume === "number" ? volume : 0.3;
   if (mute) {
