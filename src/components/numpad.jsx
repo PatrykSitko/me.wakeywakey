438d834aafa7692bd510612cccc2d4e8d86c2b79
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2019-11-07 19:04:02 +0100

    Created a custom numpad

---
+++ src/components/numpad.jsx
@@ -0,0 +1,78 @@
+// import React, { useRef, useEffect } from "react";
+
+// const FUNCTION = 1;
+
+// function Numpad({ useTriggerInputFieldState, useNumberState, ...others }) {
+//   const ref = useRef();
+//   const setNumber = useNumberState[FUNCTION];
+//   const [triggerEvent, setTriggerEvent] = useTriggerInputFieldState;
+//   useEffect(() => {
+//     if (ref && ref.current && triggerEvent) {
+//       Object.values(ref.current)[0].click();
+//       ref.current.focus();
+//       ref.current.click();
+//       setTriggerEvent(false);
+//     }
+//   }, [ref, triggerEvent, setTriggerEvent]);
+//   return (
+//     <form
+//       onSubmit={e => e.preventDefault()}
+//       onChange={e => setNumber(parseInt(`${e.target.value}`.split("").pop()))}
+//       {...{ ref, ...others }}
+//     >
+//       <input id="numpad" ref={ref} type="number" />
+//     </form>
+//   );
+// }
+
+// export default Numpad;
+import React, { useEffect } from "react";
+import "./numpad.css";
+
+const VARIABLE = 0;
+
+function Numpad({ useTriggerInputFieldState, useNumberState, ...other }) {
+  const [number, setNumber] = useNumberState;
+  const triggerEvent = useTriggerInputFieldState[VARIABLE];
+  useEffect(() => {
+    if (!triggerEvent && typeof number === "number") {
+      setNumber(undefined);
+    }
+  }, [number, setNumber, triggerEvent]);
+  return (
+    <div className="numpad" {...other} hidden={!triggerEvent}>
+      <div className="number" onClick={() => setNumber(1)}>
+        1
+      </div>
+      <div className="number" onClick={() => setNumber(2)}>
+        2
+      </div>
+      <div className="number" onClick={() => setNumber(3)}>
+        3
+      </div>
+      <div className="number" onClick={() => setNumber(4)}>
+        4
+      </div>
+      <div className="number" onClick={() => setNumber(5)}>
+        5
+      </div>
+      <div className="number" onClick={() => setNumber(6)}>
+        6
+      </div>
+      <div className="number" onClick={() => setNumber(7)}>
+        7
+      </div>
+      <div className="number" onClick={() => setNumber(8)}>
+        8
+      </div>
+      <div className="number" onClick={() => setNumber(9)}>
+        9
+      </div>
+      <div className="number" onClick={() => setNumber(0)}>
+        0
+      </div>
+    </div>
+  );
+}
+
+export default Numpad;
